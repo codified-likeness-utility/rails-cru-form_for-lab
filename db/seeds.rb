a248372c5f69f74463b6bc5a413e66c4185e9644
Ian Davies <mr.daviesian@gmail.com>
2021-03-21 23:34:25 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.destroy_all
+Genre.destroy_all
+Song.destroy_all
+
+a1 = Artist.create(name: 'Ian Davies', bio: "nothing much")
+a2 = Artist.create(name: 'Rock Johnson', bio: "nothing much")
+a3 = Artist.create(name: 'Melvin Seaburt', bio: "nothing much")
+
+g1 = Genre.create(name: 'Rock')
+g2 = Genre.create(name: 'Country')
+g3 = Genre.create(name: 'Pop')
+
+s1 = Song.create(name: 'Groovy', artist_id: a3.id, genre_id: g2.id)
+s2 = Song.create(name: 'Mmmm...hmmm', artist_id: a2.id, genre_id: g3.id)
+s3 = Song.create(name: 'Yezzur!', artist_id: a1.id, genre_id: g3.id)
